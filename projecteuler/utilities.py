fb6a28d49321ca918e5c3db8aa7f30c7264a5769
micah <micahpaul@protonmail.com>
2017-10-01 16:47:51 +0100

    Update utilities.py
    Rename utils.py to utilities.py
    Format code

--- projecteuler/utilities.py
+++ projecteuler/utilities.py
@@ -1,6 +1,8 @@
 from timeit import default_timer as timer
 
 
+# TODO: add n_primes function that returns a list of n primes
+
 def time_problem(problem):
     """runs and times a function"""
     start = timer()
@@ -13,14 +15,10 @@ def prime_sieve(n):
     sieve = [True] * n
     for i in range(3, int(n ** 0.5) + 1, 2):
         if sieve[i]:
-            sieve[i*i::2*i] = [False] * ((n-i * i-1) // (2*i)+1)
+            sieve[i * i::2 * i] = [False] * ((n - i * i - 1) // (2 * i) + 1)
     return [2] + [i for i in range(3, n, 2) if sieve[i]]
 
 
 def factors(n):
     """returns the factors of n"""
-    return [i for i in range(1, n//2+1) if not n % i] + [n]
-
-# TODO: add n_primes function that returns a list of n primes
-
-print(factors(24))
+    return [i for i in range(1, n // 2 + 1) if not n % i] + [n]
