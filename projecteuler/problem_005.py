978f86c792c63e93e8b58cdad3b46e07cf5298af
micah <micahpaul@protonmail.com>
2018-04-01 23:13:36 +0100

    refactor problem 4 - use descriptive variable names for greater readability

--- projecteuler/problem_005.py
+++ projecteuler/problem_005.py
@@ -1,22 +1,21 @@
-from projecteuler import util
 import numpy as np
+from projecteuler import util
+
+"""
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+"""
 
 
 def solution():
-    """
-    2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
-    What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
-    """
-
-    n = np.arange(1, 21)
-    i = 1
-
+    dividend = 11
+    divisors = np.arange(1, 21)
     while True:
-        x = i / n
-        if all(y.is_integer() for y in x):
-            return i
-        i += 1
+        quotients = dividend / divisors
+        if all(x.is_integer() for x in quotients):
+            return dividend
+        dividend += 1
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     assert str(solution()) == util.get_answer(5)
